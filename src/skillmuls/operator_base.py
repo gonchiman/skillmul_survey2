9020c6594581634a9c28eedf9c0b5c295be82fe8
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-02 13:50:54 +0900

    update operator_base

--- src/skillmuls/operator_base.py
+++ src/skillmuls/operator_base.py
@@ -7,31 +7,22 @@ class OperatorBase:
     ULTIMATE_STACK_MUL = None
 
     @classmethod
-    def get_battle_skill_mul(cls, skill_id: int, stack: int = 0) -> int:
-        stack_mul = 0
-        if stack > 0 :
-            if cls.BATTLE_SKILL_STACK_MUL is None:
-                raise Exception("This operator doesn't have the SKILL_STACK_MUL.")
-            else:
-                stack_mul = cls.BATTLE_SKILL_STACK_MUL * stack
-        return sum(cls.BATTLE_SKILLS[skill_id]) + stack_mul
+    def _calc_mul(cls, skill_table: dict, skill_id: int, stack: int, stack_mul: int | None) -> int:
+        extra = 0
+        if stack > 0:
+            if stack_mul is None:
+                raise ValueError("This operator doesn't support stack.")
+            extra = stack_mul * stack
+        return sum(skill_table[skill_id]) + extra
     
+    @classmethod
+    def get_battle_skill_mul(cls, skill_id: int, stack: int = 0) -> int:
+        return cls._calc_mul(cls.BATTLE_SKILLS, skill_id, stack, cls.BATTLE_SKILL_STACK_MUL)
+
     @classmethod
     def get_combo_skill_mul(cls, skill_id: int, stack: int = 0) -> int:
-        stack_mul = 0
-        if stack > 0 :
-            if cls.COMBO_SKILL_STACK_MUL is None:
-                raise Exception("This operator doesn't have the SKILL_STACK_MUL.")
-            else:
-                stack_mul = cls.COMBO_SKILL_STACK_MUL * stack
-        return sum(cls.COMBO_SKILLS[skill_id]) + stack_mul
-    
+        return cls._calc_mul(cls.COMBO_SKILLS, skill_id, stack, cls.COMBO_SKILL_STACK_MUL)
+
     @classmethod
     def get_ultimate_mul(cls, skill_id: int, stack: int = 0) -> int:
-        stack_mul = 0
-        if stack > 0 :
-            if cls.ULTIMATE_STACK_MUL is None:
-                raise Exception("This operator doesn't have the SKILL_STACK_MUL.")
-            else:
-                stack_mul = cls.ULTIMATE_STACK_MUL * stack
-        return sum(cls.ULTIMATES[skill_id]) + stack_mul
+        return cls._calc_mul(cls.ULTIMATES, skill_id, stack, cls.ULTIMATE_STACK_MUL)
