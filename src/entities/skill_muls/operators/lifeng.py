b3d5ca6bc69b091e796dcdde6aa05d1d577b0422
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-02 18:28:36 +0900

    add lifeng.py

---
+++ src/entities/skill_muls/operators/lifeng.py
@@ -0,0 +1,14 @@
+from ..operator_base import OperatorBase
+
+
+class Lifeng(OperatorBase):
+    BATTLE_SKILLS = {
+        1: [86, 86, 268]
+    }
+    COMBO_SKILLS = {
+        1: [105, 375]
+    }
+    ULTIMATES = {
+        1: [400, 400],
+        2: [400, 400, 600]
+    }
