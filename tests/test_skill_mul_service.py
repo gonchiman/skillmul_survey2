2beda847c4fc7dc65b423b7f896cdb5f6bb99a16
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-03 14:25:42 +0900

    add test_get_skill_mul_value_error_when_skill_type_is_invalid

--- tests/test_skill_mul_service.py
+++ tests/test_skill_mul_service.py
@@ -1,8 +1,15 @@
+from enum import Enum
+
+import pytest
+
 from src.services.skill_mul_service.skill_mul_service import SkillMulService
 from src.constants.operator_names import OperatorNames
 from src.constants.skill_type import SkillType
 from src.services.skill_mul_service.skill_mul_condition import SkillMulCondition
 
+
+class FakeSKillType(Enum):
+    FAKE = "fake"
 
 def test_get_skill_mul_normal():
     cond = SkillMulCondition(
@@ -12,3 +19,13 @@ def test_get_skill_mul_normal():
     )
 
     assert SkillMulService.get_skill_mul(cond) == sum([86, 86, 268])
+
+def test_get_skill_mul_value_error_when_skill_type_is_invalid():
+    cond = SkillMulCondition(
+        operator_name=OperatorNames.LIFENG,
+        skill_type=FakeSKillType.FAKE,
+        skill_id=1,
+    )
+
+    with pytest.raises(ValueError):
+        SkillMulService.get_skill_mul(cond)
