e483f2044ef70e7555cc91f7ea13f8c8983bc0b6
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-03 13:07:30 +0900

    refactor test_skill_mul_condition.py

--- tests/test_skill_mul_condition.py
+++ tests/test_skill_mul_condition.py
@@ -1,27 +1,17 @@
-from enum import Enum
-
+from constants.operator_names import OperatorNames
 from constants.skill_type import SkillType
 from services.skill_mul_service.skill_mul_condition import SkillMulCondition
 
 
-class FakeOperatorNames(Enum):
-    FAKEOPERATOR = "fake_operator"
-
-
-def test_skill_mul_condition():
-    operator_name = FakeOperatorNames.FAKEOPERATOR
-    skill_type = SkillType.BATTLE
-    skill_id = 1
-    stack = 0
-
+def test_skill_mul_condition_stores_values():
     cond = SkillMulCondition(
-        operator_name,
-        skill_type,
-        skill_id,
-        stack
+        operator_name=OperatorNames.LIFENG,
+        skill_type=SkillType.BATTLE,
+        skill_id=1,
+        stack=0,
     )
 
-    assert cond.operator_name == FakeOperatorNames.FAKEOPERATOR
+    assert cond.operator_name == OperatorNames.LIFENG
     assert cond.skill_type == SkillType.BATTLE
     assert cond.skill_id == 1
     assert cond.stack == 0
