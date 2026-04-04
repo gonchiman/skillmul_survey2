aec67f664a1d5d04b6e8eebaaab506180d84a45d
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-04 10:34:53 +0900

    update app.py

--- app.py
+++ app.py
@@ -15,17 +15,38 @@ def index():
 
 @app.route("/skill_mul", methods=["GET", "POST"])
 def skill_mul_page():
-    if request.method == "POST":
+    selected_operator_name = request.form.get("operator_name", list(OperatorNames)[0].value)
+    selected_skill_type = request.form.get("skill_type", list(SkillType)[0].value)
+    selected_skill_id = request.form.get("skill_id", "")
+    selected_stack = request.form.get("stack", "0")
+
+    operator_name = OperatorNames(selected_operator_name)
+    skill_type = SkillType(selected_skill_type)
+    skill_ids = SkillMulService.get_skill_ids(operator_name, skill_type)
+
+    skill_mul = None
+
+    if request.method == "POST" and selected_skill_id != "":
         cond = SkillMulCondition(
-            operator_name=OperatorNames(request.form["operator_name"]),
-            skill_type=SkillType(request.form["skill_type"]),
-            skill_id=int(request.form["skill_id"]),
-            stack=int(request.form["stack"] or 0)
+            operator_name=operator_name,
+            skill_type=skill_type,
+            skill_id=int(selected_skill_id),
+            stack=int(selected_stack or 0)
         )
         skill_mul = SkillMulService.get_skill_mul(cond)
-        return render_template("skill_mul_search.html", skill_mul=skill_mul)
     
-    return render_template("skill_mul_search.html")
+    return render_template(
+        "skill_mul_search.html",
+        operator_names=OperatorNames,
+        skill_types=SkillType,
+        skill_ids=skill_ids,
+        stacks=[s for s in range(0, 5)],
+        selected_operator_name=selected_operator_name,
+        selected_skill_type=selected_skill_type,
+        selected_skill_id=selected_skill_id,
+        selected_stack=selected_stack,
+        skill_mul=skill_mul,
+    )
 
 if __name__ == "__main__":
     app.run(debug=True)
