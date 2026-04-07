54af2301b064a786cba023123f5f6abc85027adc
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-07 18:15:01 +0900

    remove get_default_stack

--- src/services/skill_mul_service.py
+++ src/services/skill_mul_service.py
@@ -25,18 +25,3 @@ class SkillMulService:
             return operator_cls.get_ultimate_mul(cond.skill_id, cond.stack)
         
         raise ValueError("invalid skill type")
-    
-    @staticmethod
-    def get_default_stack(operator_name, skill_type) -> int:
-        operator_cls = OperatorRepository.get_by_id(operator_name)
-
-        if skill_type == SkillType.BATTLE:
-            return 4 if operator_cls.BATTLE_SKILL_STACK_MUL is not None else 0
-
-        if skill_type == SkillType.COMBO:
-            return 4 if operator_cls.COMBO_SKILL_STACK_MUL is not None else 0
-
-        if skill_type == SkillType.ULTIMATE:
-            return 4 if operator_cls.ULTIMATE_STACK_MUL is not None else 0
-
-        raise ValueError("invalid skill type")
