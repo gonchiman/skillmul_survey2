9825289588ef119b6b37a8c880c919a18f34d50a
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-07 16:00:01 +0900

    refactor

--- src/repositories/operator_repository.py
+++ src/repositories/operator_repository.py
@@ -1,10 +1,11 @@
+from src.entities.operator_base import OperatorBase
 from src.constants.operator_names import OperatorNames
 from src.repositories.operator_registry import OPERATORS
 
 
 class OperatorRepository:
     @classmethod
-    def get_by_id(cls, name: OperatorNames):
+    def get_by_id(cls, name: OperatorNames) -> OperatorBase:
         if name not in OPERATORS:
             raise ValueError("operator not found")
         return OPERATORS[name]
