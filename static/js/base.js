e162c6cb49f3b7ee0372ab5fcf8232a95190441f
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-09 16:34:41 +0900

    rename

--- static/js/base.js
+++
@@ -1,8 +0,0 @@
-const button = document.getElementById("menu-toggle");
-const sidebar = document.getElementById("sidebar");
-
-if (button && sidebar){
-    button.addEventListener("click", function () {
-        sidebar.classList.toggle("closed");
-    });
-}
