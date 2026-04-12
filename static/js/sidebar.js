c804de638cb0a2530d0bbaa1c484d19d104789f1
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-12 11:27:59 +0900

    update

--- static/js/sidebar.js
+++ static/js/sidebar.js
@@ -1,8 +1,17 @@
 const button = document.getElementById("menu-toggle");
 const sidebar = document.getElementById("sidebar");
 
-if (button && sidebar){
-    button.addEventListener("click", function () {
-        sidebar.classList.toggle("closed");
-    });
-}
+button.addEventListener("click", function (event) {
+    event.stopPropagation();
+    sidebar.classList.toggle("closed");
+});
+
+sidebar.addEventListener("click", function (event) {
+    event.stopPropagation();
+});
+
+document.addEventListener("click", function () {
+    if (!sidebar.classList.contains("closed")) {
+        sidebar.classList.add("closed");
+    }
+});
