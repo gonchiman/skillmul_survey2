afec21b7d635508dadc03558835739a71cac35e1
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-13 18:12:30 +0900

    add

---
+++ src/services/skill_mul_table_builder.py
@@ -0,0 +1,42 @@
+import pandas as pd
+
+from src.constants.operator_names import OperatorNames
+from src.constants.skill_type import SkillType
+from src.entities.skill_mul_condition import SkillMulCondition
+from src.services.skill_mul_service import SkillMulService
+
+
+class SkillMulTableBuilder:
+    @staticmethod
+    def get_skill_mul_table() -> pd.DataFrame:
+        data = SkillMulTableBuilder._get_data()
+        return pd.DataFrame.from_dict(
+            data,
+            orient="index",
+            columns=[
+                "battle_skill",
+                "combo_skill",
+                "ultimate"
+            ]
+        )
+
+    @staticmethod
+    def _get_data() -> dict:
+        keys = list()
+        values = list()
+
+        for operator_name in OperatorNames:
+            keys.append(operator_name)
+            skill_muls = list()
+
+            for skill_type in SkillType:
+                cond = SkillMulCondition(
+                    operator_name=operator_name,
+                    skill_type=skill_type,
+                )
+                skill_mul = SkillMulService.get_skill_mul(cond)
+                skill_muls.append(skill_mul)
+
+            values.append(skill_muls)
+
+        return dict(zip(keys, values))
