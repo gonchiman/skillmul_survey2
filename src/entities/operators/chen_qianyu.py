58ab8706d239cc8c9c3899b05f6400756ab95402
Ryo Yamamoto <legendaryyamasama@gmail.com>
2026-04-16 15:40:55 +0900

    refactor

--- src/entities/operators/chen_qianyu.py
+++ src/entities/operators/chen_qianyu.py
@@ -4,4 +4,4 @@ from src.entities.operator_base import OperatorBase
 class ChenQianyu(OperatorBase):
     BATTLE_SKILLS = (380,)
     COMBO_SKILLS = (270,)
-    ULTIMATES = (81 * 6, 1023)
+    ULTIMATES = (81*6, 1023)
